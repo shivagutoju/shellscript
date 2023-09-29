abbe158785cd079fd876dca749619c470accad5b
shivagutoju <gshivadevops.online>
2023-09-29 23:29:02 +0530

    modifications

--- 03-install-mysql.sh
+++ 03-install-mysql.sh
@@ -6,7 +6,8 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
 echo "error: Please run this script with root access"
+exit 1
 else
 echo "you are root user"
 fi
-yum install git -y
+yum install mysql -y

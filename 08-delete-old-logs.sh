91ae2e3717529bb94f92a242b82df6641d26ccd6
shivagutoju <gshivadevops.online>
2023-10-02 18:41:11 +0530

    modifications

--- 08-delete-old-logs.sh
+++ 08-delete-old-logs.sh
@@ -10,7 +10,7 @@ echo "$FILES_TO_DELETE"
 
 while read line
 do
-echo "deleting $line" &> $LOGFILE
+echo "deleting $line" &>> $LOGFILE
 rm -rf $line    
 #we write statement here
 done <<< $FILES_TO_DELETE

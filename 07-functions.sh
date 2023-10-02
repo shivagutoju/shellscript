e200dfda759223ea3c877bac162b48e5ae659ee4
shivagutoju <gshivadevops.online>
2023-10-02 10:32:10 +0530

    modifications

--- 07-functions.sh
+++ 07-functions.sh
@@ -3,3 +3,14 @@ DATE=$(date +%F)
 SCRIPT_NAME=$0
 LOGFILE=/tmp/SCRIPT_NAME-$DATE.log
 
+#this function should validate  the previous command and inform user it is sucess or failure
+validate(){
+    #$1--it will receive the orgument1
+    if [ $1 -ne 0 ]
+    then
+    echo -e "$2 ... $R FAILURE $N"
+    exit1
+    else
+    echo -e "$2 ... $G SUCESS $N"
+    fi
+}

8f821a6102e9476315dc43eb0401eeb520557156
shivagutoju <gshivadevops.online>
2023-10-02 18:33:02 +0530

    modifications

--- 08-delete-old-logs.sh
+++ 08-delete-old-logs.sh
@@ -7,3 +7,10 @@ LOGFILE=$LOGSDIR/$0-$DATE.log
 FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +14)
 
 echo "$FILES_TO_DELETE"
+
+while read line
+do
+echo "deleting $line" &> $LOGFILE
+rm -rf $line    
+#we write statement here
+done < $FILES_TO_DELETE

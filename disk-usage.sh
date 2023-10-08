715459e920d9a178fd60c434e2d459cc77653f94
shivagutoju <gshivadevops.online>
2023-10-08 14:58:29 +0530

    modifications

--- disk-usage.sh
+++ disk-usage.sh
@@ -13,3 +13,13 @@ R="\e[31m"
 G="\e[32m"
 N="\e[33m"
 
+DISK_USAGE=$(df -HT | grep -vE 'tmpfs|Filesystem')
+
+DISK_USAGE_THRESHOLD=1
+#ifs=internal field seperator is space.
+
+while IFS= read line
+do
+echo "OUTPUT: $line"
+
+done >>> $DISK_USAGE

57c7c232dd497a682fc2c9a031d70ce9b963bf18
shivagutoju <gshivadevops.online>
2023-10-08 17:28:20 +0530

    modifications

--- disk-usage.sh
+++ disk-usage.sh
@@ -16,10 +16,22 @@ N="\e[33m"
 DISK_USAGE=$(df -HT | grep -vE 'tmpfs|Filesystem')
 
 DISK_USAGE_THRESHOLD=1
-#ifs=internal field seperator is space.
+message=
+#IFS=internal field seperator is space.
 
 while IFS= read line
 do
-echo "OUTPUT: $line"
+#this command line give you usage in number format for comparison
 
+usage=$(echo $line | awk '{print $6}' | cut -d % -f1)
+#this command will give us parition
+partition=$echo $line | awk '{print $1}
+# now you need to check wheter it is more than it is threshold or not
+if [ $usage -gt $DISK_USAGE_THRESHOLD ];
+then
+#if you will give = then append will apply, second time also function will work
+messege+="HIGH DISK USAGE ON $partition: $usage"
+fi
 done <<< $DISK_USAGE
+
+echo "message: $message"

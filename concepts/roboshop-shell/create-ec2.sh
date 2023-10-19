52768d46343ad4febe9d8fdb863a99456430ad4a
shivagutoju <gshivadevops.online>
2023-10-19 23:13:06 +0530

    added servers script

---
+++ concepts/roboshop-shell/create-ec2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+
+
+for i in "${NAMES[@]}"
+
+do
+    echo "NAME: $i"
+
+    done

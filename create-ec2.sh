ca507be875d621619d730b3de52b8c1920f60d78
shivagutoju <gshivadevops.online>
2023-10-22 18:18:05 +0530

    added servers script

--- create-ec2.sh
+++ create-ec2.sh
@@ -4,7 +4,7 @@ NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping"
 INSTANCE_TYPE=""
 IMAGE_ID=ami-03265a0778a880afb
 SECURITY_GROUP_ID=sg-07e1ea518d41fdfed
-DOMAIN_NAME=gshivadevops.online    
+DOMAIN_NAME=joindevops.online    
 
 #if mysql or mongodb instance_type should be t3.medium, for all others t2.micro
 

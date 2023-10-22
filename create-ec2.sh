c1a92272b102b15df1249780ce16ce93c4a5d5ef
shivagutoju <gshivadevops.online>
2023-10-22 19:45:19 +0530

    added servers script

--- create-ec2.sh
+++ create-ec2.sh
@@ -4,37 +4,36 @@ NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping"
 INSTANCE_TYPE=""
 IMAGE_ID=ami-03265a0778a880afb
 SECURITY_GROUP_ID=sg-07e1ea518d41fdfed
-DOMAIN_NAME=gshivadevops.online    
+DOMAIN_NAME=gshivadevops.online
 
-#if mysql or mongodb instance_type should be t3.medium, for all others t2.micro
+# if mysql or mongodb instance_type should be t3.medium , for all others it is t2.micro
 
 for i in "${NAMES[@]}"
-
-do
-if [[ $i == "mongodb" || $i == "mysql" ]]
-then
-     INSTANCE_TYPE="t3.medium"
-     else
-     INSTANCE_TYPE="t2.micro"
-     fi
+do  
+    if [[ $i == "mongodb" || $i == "mysql" ]]
+    then
+        INSTANCE_TYPE="t3.medium"
+    else
+        INSTANCE_TYPE="t2.micro"
+    fi
     echo "creating $i instance"
-    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids 
-    $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].privateIpaddress')
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
     echo "created $i instance: $IP_ADDRESS"
 
-    $ aws route53 change-resource-record-sets --hosted-zone-id Z05684162U49B9NYH4WVI \ --change-batch '
+    aws route53 change-resource-record-sets --hosted-zone-id Z05684162U49B9NYH4WVI --change-batch '
     {
-          
             "Changes": [{
             "Action": "CREATE",
                         "ResourceRecordSet": {
-                              "Name": "'$i.$DOMAIN_NAME'",
-                              "Type": "A",
-                              "TTL": 300,
-                              "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
-                         }}]
-}
-'
+                            "Name": "'$i.$DOMAIN_NAME'",
+                            "Type": "A",
+                            "TTL": 300,
+                            "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
+                        }}]
+    }
+    '
 done
 
-
+# imporvement
+# check instance is already created or not
+# update route53 record
